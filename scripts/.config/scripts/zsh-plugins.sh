4342805b047212a813fa589a074595ec7ee53fb3
dellie <delsterone@gmail.com>
2025-03-30 15:37:18 -0500

    sync

---
+++ scripts/.config/scripts/zsh-plugins.sh
@@ -0,0 +1,3 @@
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git /usr/share/zsh-autosuggestions 
+sudo git clone https://github.com/marlonrichert/zsh-autocomplete /usr/share/zsh/plugins/zsh-autocomplete

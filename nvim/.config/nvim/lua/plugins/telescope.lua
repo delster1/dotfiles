0727aeefbe1e18b9c53df78d23f896d1d3c0db2c
dellie <delsterone@gmail.com>
2025-11-14 11:29:26 -0600

    add telescope stuff

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		config = function()
+			require("telescope").load_extension("git_branch")
+		end,
+		-- or                              , branch = '0.1.x',
+	},
+	{
+		"mrloop/telescope-git-branch.nvim",
+	},
 }

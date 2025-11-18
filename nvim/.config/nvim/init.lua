545328645bbcbedeab96e61b66091e6c5afef4ed
dellie wright <delsterone@gmail.com>
2025-11-17 21:32:12 -0600

    save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,36 @@
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.mapleader = ","
+vim.opt.termguicolors = true
+
+--THEME ROTATE LEADERrt
+local themes = { "catppuccin", "tokyonight", "gruvbox", "everforest" }
+local current_theme_index = 1  -- Start with the first theme
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true, silent = true })
+-- Function to set a theme
+local function set_theme(theme)
+    local success, _ = pcall(vim.cmd, "colorscheme " .. theme)
+    if success then
+        print("Switched to theme: " .. theme)
+    else
+        print("Failed to load theme: " .. theme)
+    end
+end
+
+-- Function to rotate themes
+local function rotate_theme()
+    current_theme_index = (current_theme_index % #themes) + 1
+    set_theme(themes[current_theme_index])
+end
+
+-- Set initial theme
+set_theme(themes[current_theme_index])
+
+-- Keybinding to rotate themes
+vim.keymap.set("n", "<leader>rt", rotate_theme, { desc = "Rotate Themes" })
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -11,13 +44,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local opts = {}
 
-require("config.vim-options")
 require("lazy").setup("plugins")
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require("config.keymaps")
-  end,
-})
-
+vim.notify = require("notify")
+vim.cmd("colorscheme everforest")
--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,67 +1,9 @@
 return {
-{
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-  config = function()
-    require("everforest").setup({
-      -- Your config here
-    })
-  end,
-},
-{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
-,{ "EdenEast/nightfox.nvim" }, -- lazy
-{
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    -- Enable theme
-    require('onedark').load()
-  end
-},
- {
-	"rose-pine/neovim",
-  variant = "moon",
-	name = "rose-pine",
-},
-{
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-},
-{
-  'everviolet/nvim', name = 'evergarden',
-  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
-      accent = 'green',
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = 'none' },
-      float = {
-        color = 'mantle',
-        solid_border = false,
-      },
-      completion = {
-        color = 'surface0',
-      },
-    },
-  }
-},
-{
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('nordic').load()
-    end
-  },
-  { "Mofiqul/dracula.nvim" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "folke/tokyonight.nvim", lazy = true },
+	{ "morhetz/gruvbox", lazy = true },
+	{
+		"neanias/everforest-nvim",
+		lazy = true,
+	},
 }

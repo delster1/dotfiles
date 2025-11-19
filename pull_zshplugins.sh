7080a9e9edfd22fe58cb0db2fd9013dbc7b83b59
dellie wright <delsterone@gmail.com>
2025-11-18 19:30:41 -0600

    pull zsh plugins

---
+++ pull_zshplugins.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/marlonrichert/zsh-autocomplete ~/.oh-my-zsh/plugins/zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
